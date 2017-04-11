945ee6a3d51e07bd4cacf67285f46ebbf61be90a
Sharad Boni <sharadboni@gmail.com>
2017-04-11 17:29:35 -0400

    Update Message.py

--- src/Message.py
+++ src/Message.py
@@ -1,3 +1,5 @@
+#planning to encrypt and decrypt the messages here only in the classes Message and UnMessage
+
 import time
 import json
 
@@ -17,14 +19,26 @@ class Message():
         self.time = time.time()
         self.username=username
         self.json = json.dumps(
-            {'type': self.type,'usernmae': self.username, 'msg': self.msg, 'time': self.time})
+            {'type': self.type,'username': self.username, 'msg': self.msg, 'time': self.time})
 
-    def print_msg(self):
-        self.json=json.loads(self.json)
-        print self.json[msg]
-        
-
-    def get_msg(self):
-        return self.json
+class UnMessage():
     
+    def __init__(self,data):
+        self.json = json.loads(data)
+        self.type = self.json['type']
+        self.msg = self.json['msg']
+        self.time = self.json['time']
+        self.username=self.json['username']
         
+
+    def get_message(self):
+        return self.msg
+    
+    def get_type(self):
+        return self.type
+    
+    def get_time(self):
+        return self.time
+    
+    def get_username(self):
+        return self.username
