47765e72304d10b3d04e69af562f5839cace5671
talhaozcan <a.talhaozcan@gmail.com>
2017-04-15 16:51:55 -0400

    server.config, msg worker wor Message class

--- src/Message.py
+++ src/Message.py
@@ -4,18 +4,45 @@
 import time
 import json
 import Crypt_Functions
+import multiprocessing
+
 
 # message types
-SIGNIN = 'SIGN-IN'
+LOGIN = 'LOGIN'
 LIST = 'LIST'
 MESSAGE = 'MESSAGE'
+LOGOUT = 'LOGOUT'
 EXIT = 'EXIT'
+COMM_REQ = 'COMMUNICATION-REQUEST'
+
 GET_PUB_KEY='GET_KEY'
 ESTAB_KEY='ESTAB_KEY'
 
+
+# waits for coming messages and puts them into a queue
+class Msg_Worker(multiprocessing.Process):
+
+    def __init__(self, sock, queue):
+
+        self.sock = sock
+        # multiprocessing queue to store active user info
+        self.queue = queue
+
+    def run(self):
+
+        while True:
+
+            try:
+                # msg and source ip-port
+                msg, addr = self.sock.recvfrom(4096)
+
+                self.queue.put((msg, addr))
+
+
+
 class Message():
 
-    def __init__(self, _type,username,msg=None):
+    def __init__(self, _type, username, msg=None):
 
         self.type = _type
         self.msg = msg
